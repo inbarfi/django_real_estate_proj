9e79c5214ffcada21d779405f978fed6a7ad2adf
inbarfi <inbarfi.dev@gmail.com>
2021-08-29 17:21:56 +0300

    real_eatate_finished_d1

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
